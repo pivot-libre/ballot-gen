dfac8628c7c68b6849fa51e16a878e4974e41c31
tylerharter <tylerharter@gmail.com>
2018-06-19 13:13:15 -0500

    generate alchemy graphs

---
+++ visualize.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import os, sys, json, shutil
+import graph_gen
+
+CODE_DIR = os.path.dirname(__file__)
+
+def generate_alchemy_graph(graph, outdir):
+    # convert to alchemy format
+    alchemy = {'nodes': [], 'edges': []}
+    for node in graph['nodes']:
+        caption = node
+        alchemy['nodes'].append({'id': node, 'caption': caption})
+    for edge in graph['edges']:
+        alchemy['edges'].append({'source': edge[graph_gen.SOURCE], 'target': edge[graph_gen.TARGET]})
+    js = json.dumps(alchemy)
+
+    if os.path.exists(outdir):
+        print 'Directory %s already exists' % outdir
+        return
+
+    os.makedirs(outdir)
+    src = os.path.join(CODE_DIR, 'alchemy')
+    dst = os.path.join(outdir, 'alchemy')
+    shutil.copytree(src, dst)
+    with open(os.path.join(CODE_DIR, 'alchemy-template.htm')) as f:
+        html = f.read()
+    html = html.replace('<GRAPH_DATA>', js)
+    with open(os.path.join(outdir, 'graph.htm'), 'w') as f:
+        f.write(html)
+
+    print 'generated in %s' % os.path.join(outdir, 'graph.htm')
+
+def main():
+    outdir = sys.argv[1]
+    graph = {'nodes': ['0','1','2'],
+             'edges': [{'source': '0', 'target': '1'},
+                       {'source': '1', 'target': '2'},
+                       {'source': '2', 'target': '0'}]}
+    generate_alchemy_graph(graph, outdir)
+
+if __name__ == '__main__':
+    main()
